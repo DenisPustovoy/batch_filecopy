4fa3d2381fb31001ab42665426e236ea147c68f4
DenisPustovoy <DenisPustovoy@ukr.net>
2019-10-28 20:41:43 +0300

    update1

--- file_cp.sh
+++ file_cp.sh
@@ -1,8 +1,8 @@
-@echo off
+@echo on
 
-Set first_folder=F:\Test1
-Set second_folder=F:\Test2
-Set third_folder=F:\Test3
+Set first_folder="F:\Test Env\Videos on Server"
+Set second_folder="F:\Test Env\Unorganized Thumbs"
+Set third_folder="F:\Test Env\Organized Thumbs"
 
 for /f "tokens=*" %%A in ('dir /b /a:d %first_folder%') do (
 
@@ -14,17 +14,13 @@ for /f "tokens=*" %%A in ('dir /b /a:d %first_folder%') do (
 			
 			for /f "delims= eol=" %%D in ('dir %first_folder%\%%A\%%B\%%C /b') do (
 			
-				for /f "delims=\ tokens=3-5" %%f in ("%first_folder%\%%A\%%B\%%C\%%D") do (
+				for /f "delims=\ tokens=4-6" %%f in ("%first_folder%\%%A\%%B\%%C\%%D") do (
 				
-					IF EXIST %second_folder%\%%~nD.jpg (
-					
 						IF EXIST %third_folder%\%%f-%%g-%%h (
 						
-							move %second_folder%\%%~nD.jpg %third_folder%\%%f-%%g-%%h\%%~nD.jpg
+							move %second_folder%\"%%~nD.jpg" %third_folder%\%%f-%%g-%%h\"%%~nD.jpg"
 							
 						)
-						 
-					) 				
 				)
 			)
 		)  
